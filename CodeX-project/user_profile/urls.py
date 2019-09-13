35bb9e39597f12039aca0bd37bb7281d9ab9a755
jijinkh <jijinkharidas10@gmail.com>
2019-09-13 13:17:25 +0530

    templates added

--- CodeX-project/user_profile/urls.py
+++ CodeX-project/user_profile/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('login',views.login,name="login"),
+    path('register',views.register,name="register"),
+
 ]
